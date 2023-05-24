c9a9e1341cfa505ef8792b00e41e03b4134850c7
Ronnie Rios <ronniesmail14@gmail.com>
2023-05-24 15:20:39 -0500

    adjust model with pre

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,17 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     username: {
       type: String,
-      required: [true, "Username is required"]
+      required: [true, "Username is required"],
     },
     password: {
       type: String,
       required: [true, "Password is required"],
-      minlength: [5, "Password must be 5 characters or longer"]
-    }
-  }, {timestamps: true});
+      minlength: [5, "Password must be 5 characters or longer"],
+    },
+  },
+  { timestamps: true }
+);
 
-  const User = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", UserSchema);
 
-  module.exports = User
+module.exports = User;
+
+UserSchema.virtual("confirmPassword")
+  .get(() => this._confirmPassword)
+  .set((value) => (this._confirmPassword = value));
+
+UserSchema.pre("validate", (next) => {
+  if (this.password !== this.confirmPassword) {
+    this.invalidate("confirmPassword", "Passwords Must Match");
+  }
+  next();
+});
+
+UserSchema.pre('save', async (next) => {
+  this.password = await bcrypt.hash(this.password, 7);
+  next()
+})
