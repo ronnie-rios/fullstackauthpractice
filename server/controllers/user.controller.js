d2e3eb0166571f3b2babb07377987678430bf326
Ronnie Rios <ronniesmail14@gmail.com>
2023-05-25 19:19:02 -0500

    registuer user func add to controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,2 +1,15 @@
 const User = require('../models/user.model');
 
+const registerUser = async (req, res) => {
+    try {
+        const user = await User.create(req.body)
+        return res.json({ message:'success', user: user});
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+}
+
+
+module.exports={
+    registerUser
+}
