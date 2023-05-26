604cf06f7fd549e1d1c00f387de359884be7c4e4
Ronnie Rios <ronniesmail14@gmail.com>
2023-05-25 19:23:43 -0500

    cookieparser adjust cors"

--- server/server.js
+++ server/server.js
@@ -7,6 +7,13 @@ const PORT = 9122;
 require('../config/mongoose.config');
 require('dotenv').config();
 
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+
+
+
 app.use(cors());
 app.use(express.json(), express.urlencoded({ extended: true }));
 
