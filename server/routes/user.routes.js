8262570d0c85acf8e536dd8f19885eb359165d0a
Ronnie Rios <ronniesmail14@gmail.com>
2023-05-30 09:30:30 -0500

    routes added

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const UserController = require('../controllers/user.controller');
+const { authenticate } = require('../config/jwt.config');
 
 const userRoutes = express.Router();
+
+
+module.exports = app => {
+    userRoutes.post('/api/register', UserController.register);
+    userRoutes.post('/api/login', UserController.login);
+    userRoutes.post('/api/users', authenticate, UserController.getAll);
+
+}
 
 module.exports = { userRoutes }
