cf7da273c1a4ceab972896699c5dcae71bab971c
Ronnie Rios <ronniesmail14@gmail.com>
2023-05-31 15:21:10 -0500

    create user

--- serverpractice/controllers/user.controller.js
+++ serverpractice/controllers/user.controller.js
@@ -12,6 +12,22 @@ const getAllUsers = async (req, res) => {
     }
 };
 
+const createUser = async (req,res) => {
+    try {
+        const user = await User.create(req.body);
+        //create a token
+        const token = jwt.sign({
+            //comes from the user._id from the user model
+            id: user._id
+        }, process.env.SECRET_KEY);
+
+        return res.cookie('token', token, { httpOnly: true }).json({ message:'created a user', user: user})
+    } catch (error) {
+        res.status(400).json({ error: error })
+    }
+}
+
 module.exports = {
-    getAllUsers
+    getAllUsers,
+    createUser
 }
