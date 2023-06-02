58a1ab7a76d219cce53833fec26bfa228e6d8f61
Ronnie Rios <ronniesmail14@gmail.com>
2023-06-02 16:18:12 -0500

    note controller

--- server/controllers/note.controller.js
+++ server/controllers/note.controller.js
@@ -1,7 +1,21 @@
-const User = require('../models/user.model');
-const Note = require('../models/notes.model');
+const User = require("../models/user.model");
+const Note = require("../models/notes.model");
 
-const postNote = async (req,res) => {
+const postNote = async (req, res) => {
+  try {
     const newNote = await Note.create(req.body);
-   
+    const foundUser = await User.findOneAndUpdate(
+      { id: req.params.id },
+      { $push: { notes: newNote._id } },
+      { new: true }
+    );
+    res.json(foundUser);
+  } catch (error) {
+    res.status(500).json({ error: error })
+  }
+};
+
+
+module.exports = {
+    postNote
 }
