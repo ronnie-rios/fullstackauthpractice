319a4973b6127fc27d75e09d070a879741aaca4d
Ronnie Rios <ronniesmail14@gmail.com>
2023-06-08 15:17:53 -0500

    user routes comments for brevity

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,12 +2,17 @@ const express = require("express");
 const UserController = require("../controllers/user.controller");
 const { authenticate } = require("../config/jwt.config");
 
+//set a variable that uses Router method
 const userRoutes = express.Router();
-
+//api/users/register
 userRoutes.post("/register", UserController.registerUser);
+//api/users/login
 userRoutes.post("/login", UserController.loginUser);
+//api/users/
 userRoutes.get("/", authenticate, UserController.getAll);
+//api/users/:id
 userRoutes.get('/:id', authenticate, UserController.getOneUser);
+//api/users/logout
 userRoutes.delete('/logout', UserController.logout);
 
 module.exports = { userRoutes };
--- serverpractice/server.js
+++ serverpractice/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { userRoutes } = require('./routes/user.routes');
+const noteRoutes  = require('./routes/notes.routes')
 
 const app = express();
 
@@ -15,5 +16,6 @@ app.use(cookieParser());
 app.use(express.json(), express.urlencoded({ extended: true }));
 
 app.use('/api/users', userRoutes)
+app.use('/api/notes', noteRoutes)
 
 app.listen(() => console.log(`server running on ${PORT}`))
